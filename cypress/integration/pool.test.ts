813593b30e1ab9bc070ef3ee676e173c82f61690
CryptoSnowPrince <topdirector2017@gmail.com>
2022-09-12 01:03:54 +0000

    update

--- cypress/integration/pool.test.ts
+++
@@ -1,8 +0,0 @@
-describe('Pool', () => {
-  beforeEach(() => cy.visit('/pool'))
-
-  it('import pool links to /import', () => {
-    cy.get('#import-pool-link').click()
-    cy.url().should('contain', '/find')
-  })
-})
