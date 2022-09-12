f4ce8501f32d17d8d1476b9575624a64dcba4b71
CryptoSnowPrince <topdirector2017@gmail.com>
2022-09-13 04:13:36 +0900

    update CardNav

--- src/components/CardNav/index.tsx
+++ src/components/CardNav/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
 import { ButtonMenu, ButtonMenuItem } from '@pancakeswap/uikit'
 import { useTranslation } from '@pancakeswap/localization'
+import { NextLinkFromReactRouter } from 'components/NextLink'
 
 const StyledNav = styled.div`
   margin-bottom: 40px;
@@ -15,16 +15,14 @@ function Nav({ activeIndex = 0 }: { activeIndex?: number }) {
   return (
     <StyledNav>
       <ButtonMenu activeIndex={activeIndex} scale="sm" variant="subtle">
-        {/* <ButtonMenuItem id="swap-nav-link" to="/swap" as={Link}> */}
-        <ButtonMenuItem id="swap-nav-link" as="a">
+        <ButtonMenuItem id="swap-nav-link" to="/swap" as={NextLinkFromReactRouter}>
           {t('Swap')}
         </ButtonMenuItem>
-        {/* <ButtonMenuItem id="pool-nav-link" to="/pool" as={Link}> */}
-        <ButtonMenuItem id="pool-nav-link" as="a">
+        <ButtonMenuItem id="pool-nav-link" to="/pool" as={NextLinkFromReactRouter}>
           {t('Liquidity')}
         </ButtonMenuItem>
       </ButtonMenu>
-    </StyledNav>
+    </StyledNav >
   )
 }
 
