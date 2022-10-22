3fb2db5373285db24eda6b9650c7e20d80f3ac18
CryptoSnowPrince <topdirector2017@gmail.com>
2022-10-22 03:37:18 +0000

    update

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Button, Heading, Text, LogoIcon, Image } from '@pancakeswap/uikit'
+import { Button, Heading, Text, Image } from '@pancakeswap/uikit'
 import Page from 'components/Layout/Page'
 import { useTranslation } from '@pancakeswap/localization'
 import Link from 'next/link'
@@ -19,7 +19,7 @@ const NotFound = ({ statusCode = 404 }: { statusCode?: number }) => {
     <Page>
       <StyledNotFound>
         {/* <LogoIcon width="64px" mb="8px" /> */}
-        <Image height={100} width={300} src={'/images/SSG/SSG_logo.png'} />
+        <Image height={100} width={300} src='/images/SSG/SSG_logo.png' />
         <Heading scale="xxl">{statusCode}</Heading>
         <Text mb="16px">{t('Oops, page not found.')}</Text>
         <Link href="/" passHref>
